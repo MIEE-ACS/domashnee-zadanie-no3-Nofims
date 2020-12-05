aad38ccef5fa856b7f138f3752c60955e7b66cf6
Nofims <eld170916@icloud.com>
2020-12-05 20:15:25 +0300

    Сделал неполностью дз3 , не получилось для последовательности знаков

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btncount_Click(object sender, RoutedEventArgs e)
+        {
+            int i = 0;
+            string sr = tb1.Text;
+            string[] s = sr.Split('.', '!', '?');
+            i = s.Length;
+
+
+            tb2.Text = string.Format("{0}", i - 1);
+        }
+    }
+}
